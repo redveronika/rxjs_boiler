bf1908fc5fdc9f3e43f8f8a6f87dd07730a1ceae
redveronika <voronovaveronika@gmail.com>
2017-08-10 11:59:05 +0300

    Added observables from scratch

--- src/scratch-observables.js
+++ src/scratch-observables.js
@@ -2,4 +2,31 @@ import Rx from 'rxjs/Rx';
 
 export default function scratchObservables () {
 
+    const source$ = new Rx.Observable(observer => {
+        console.log('Creating observable')
+
+        observer.next('Hello World');
+        observer.next('Another value');
+
+        observer.error(new Error('Error: error'));
+
+        setTimeout(() => {
+            observer.next('Yet another value');
+            observer.complete();
+        }, 3000)
+    });
+
+    source$
+        .catch(error => Rx.Observable.of(error))
+        .subscribe(
+        value => {
+            console.log(value)
+        },
+        error => {
+            console.error(error)
+        },
+        complete => {
+            console.log('Completed')
+        }
+    )
 }
